62b15f9673f3328ff8d22114cb47b0c240323313
1151Kacka <1151@student.itgymnazium.cz>
2025-11-30 23:06:52 +0100

    5

--- login.php
+++ login.php
@@ -9,22 +9,18 @@ use League\OAuth2\Client\Provider\Google;
 // --- 2. KONFIGURACE ---
 // TYTO HODNOTY MUSÍ BÝT STEJNÉ JAKO V GOOGLE CLOUD CONSOLE 
 $provider = new Google([
-    'clientId'     => '96149857284-hd9skkhktlkq4s6s7n9dd3k0dbr5bevk.apps.googleusercontent.com',        
+    'clientId'     => '96149857284-hd9skkhktlkq4s6s7n9dd3k0dbr5bevk.apps.googleusercontent.com ',        
     'clientSecret' => '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',     
     'redirectUri'  => 'http://localhost/bojovka/callback.php', 
 ]);
 
 // 3. Generování URL pro přesměrování na Google
 $authUrl = $provider->getAuthorizationUrl([
-    'scope' => [
-        'email', // Oprávnění pro získání e-mailové adresy
-        'profile' // Oprávnění pro získání jména a profilového obrázku
-    ],
-    // Důležité: 'offline' požádá o Refresh Token, který potřebuješ pro trvalý přístup
-    'access_type' => 'offline', 
-    'prompt' => 'consent' // Zajišťuje, že uživatel uvidí obrazovku souhlasu (i když už se přihlásil dříve)
+    'scope' => ['email', 'profile'],
+    'access_type' => 'offline',
+    'prompt' => 'select_account',  // Zobrazí výběr účtu
+    'login_hint' => ''  // Vymaže předvyplněný email
 ]);
-
 // 4. Uložení bezpečnostního 'state' do session
 // Toto zabraňuje CSRF útokům. Google jej odešle zpět a my ho ověříme v callback.php.
 $_SESSION['oauth2state'] = $provider->getState();
